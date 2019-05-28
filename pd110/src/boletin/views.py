55ac3ccd650c7caaa425237673011865d5f1e908
DiegoGonzalezElias <diegogonzalezelias@gmail.com>
2019-05-28 23:14:44 +0200

    video 38
    video 38

--- pd110/src/boletin/views.py
+++ pd110/src/boletin/views.py
@@ -9,7 +9,7 @@ from .models import Registrado
 def inicio(request):
 	titulo = "Bienvenidos"
 	if request.user.is_authenticated():
-		titulo = "Bienvenido %s" %(request.user)
+		titulo = "Bienvenid@ %s" %(request.user)
 	form = RegModelForm(request.POST or None)
 
 	context = {
@@ -42,6 +42,12 @@ def inicio(request):
 		#abc2 = form_data.get("nombre")
 		#obj = Registrado.objects.create(email=abc, nombre=abc2)
 	
+
+	if request.user.is_authenticated() and request.user.is_staff:
+		context = {
+			"queryset": ['abc','123'],
+		}
+
 	return render(request, "inicio.html",context)
 
 def contact(request):
